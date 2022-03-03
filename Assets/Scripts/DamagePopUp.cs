5d93e18c02665346ae5fbd0ed6c52afb2b1d5e62
MarkWeeber <61727020+MarkWeeber@users.noreply.github.com>
2022-03-03 23:17:18 +0600

    damge pop up complete

--- Assets/Scripts/DamagePopUp.cs
+++ Assets/Scripts/DamagePopUp.cs
@@ -2,12 +2,22 @@
 using TMPro;
 
 namespace Platformer.Inputs
-{// https://www.youtube.com/watch?v=iD1_JczQcFY
+{
     public class DamagePopUp : MonoBehaviour
     {
+        public float fadeTime = 1f;
+        public float moveSpeed = 3f;
+        public float fadeSpeed = 1f;
+        public Vector3 Offset = Vector3.zero;
+        private static Vector3 offset;
+        
+        private Color textColor;
         public static DamagePopUp Create(Vector3 pos, float damage, float lifetime = 2f)
         {
-            Transform trans = Instantiate(GameAssets.instance.damageCounter, pos, Quaternion.identity);
+            pos += offset;
+            Transform trans = Instantiate(  GameAssets.instance.damageCounter, 
+                                            pos,
+                                            Quaternion.identity);
             DamagePopUp damagePopUp = trans.GetComponent<DamagePopUp>();
             TextMeshPro textMeshPro = trans.GetComponent<TextMeshPro>();
             damagePopUp.PopUp(damage);
@@ -18,11 +28,34 @@ namespace Platformer.Inputs
 
         void Awake()
         {
+            offset = Offset;
             textMeshPro = GetComponent<TextMeshPro>();
         }
         public void PopUp(float damage)
         {
-            textMeshPro.SetText(damage.ToString());
+            Color clr;
+            if(damage < 0)
+                clr = Color.red;
+            else
+                clr = Color.green;
+            textMeshPro.SetText(Mathf.Abs(damage).ToString());
+            textMeshPro.faceColor = clr;
+            textColor = textMeshPro.color;
+        }
+
+        private void Update()
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+            fadeTime -= Time.deltaTime;
+            if(fadeTime < 0)
+            {
+                textColor.a -= fadeSpeed * Time.deltaTime;
+                textMeshPro.color = textColor;
+                if(textColor.a < 0 )
+                {
+                    Destroy(gameObject);
+                }
+            }
         }
     }
 }
